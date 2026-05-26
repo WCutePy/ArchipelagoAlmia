66aa36850f20602aa6f1cc316f743a1e40ac4e06
wouter <post4wouter.t@gmail.com>
2026-05-26 10:10:08 +0200

    moved it so mission 3 should end at mission 3, and nothing after is in logic

---
+++ worlds/pokemon_ranger_soa/randomize.py
@@ -0,0 +1,24 @@
+"""
+TODO, create randomizer functions,
+for now a non exhaustive list of potential script moved/spawned pokémon.
+
+school tutorial:
+ - running bidoof (might not be on the map)
+ - 4 ghastly basement (on map!
+ - slakoth from tree (likely on map)
+ - tangrowth during graduation (not on map)
+
+nabiki beach
+ - starter partners
+
+mission 2 marine cave
+ - entrance zubat attacking you (on map)
+ - circle running (on map)
+
+quest 1
+ - milktank
+
+mission 3
+ - 4 budew that jump out
+ - maybe happiny related
+"""
